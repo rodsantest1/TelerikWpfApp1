2ac37d6a374a1d56f350f8f5e79c67d701ae969c
Rodney <rodney@rodchar.onmicrosoft.com>
2021-08-04 23:05:34 -0400

    MVP for Telerik Excel document processing

---
+++ Readme.md
@@ -0,0 +1,4 @@
+
+# Telerik Document Processing Example  
+
+[[Go]](https://docs.telerik.com/devtools/document-processing/libraries/radspreadprocessing/getting-started)  
--- TelerikWpfApp1/MainWindow.xaml.cs
+++ TelerikWpfApp1/MainWindow.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Telerik.Windows.Documents.Spreadsheet.FormatProviders;
+using Telerik.Windows.Documents.Spreadsheet.FormatProviders.OpenXml.Xlsx;
+using Telerik.Windows.Documents.Spreadsheet.Model;
 
 namespace TelerikWpfApp1
 {
@@ -22,6 +16,31 @@ namespace TelerikWpfApp1
         public MainWindow()
         {
             InitializeComponent();
+
+            /* Step 1 - Create workbook */
+            Workbook workbook = new Workbook();
+            Worksheet worksheet = workbook.Worksheets.Add();
+
+            /* Step 2 - Set value of cell */
+            CellSelection selection = worksheet.Cells[1, 1]; //B2 cell 
+            selection.SetValue(07);
+
+            /* Step 3 - Export to xlsx */
+            string fileName = "SampleFile.xlsx";
+
+            IWorkbookFormatProvider formatProvider = new XlsxFormatProvider();
+
+            using (Stream output = new FileStream(fileName, FileMode.Create))
+            {
+                formatProvider.Export(workbook, output);
+            }
+
+            StatusLabel.Content = $"Excel document processing completed normally on {DateTime.Now}";
+        }
+
+        private void OpenDirectoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(@"..\Debug");
         }
     }
 }
