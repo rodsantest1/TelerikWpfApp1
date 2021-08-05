98419d8069eadc78a59b8050b32165df47f5f76e
Rodney Santiago <rodsan.724@outlook.com>
2021-08-04 23:45:03 -0400

    Added busy indicator and improved UI a little. Added Task Async/Await

--- Readme.md
+++ Readme.md
@@ -1,4 +1,10 @@
 
 # Telerik Document Processing Example  
 
-[[Go]](https://docs.telerik.com/devtools/document-processing/libraries/radspreadprocessing/getting-started)  
+8/4/21  
+
+Telerik Document Processing Site [[Go]](https://docs.telerik.com/devtools/document-processing/libraries/radspreadprocessing/getting-started)  
+
+Decimal Places and Spaces [[Go]](https://docs.telerik.com/devtools/document-processing/libraries/radspreadprocessing/features/format-codes#:~:text=(Space%20character)-,Decimal%20Places%20and%20Spaces,-With%20the%20number)  
+
+Number Formatting [[Go]](https://docs.telerik.com/devtools/document-processing/libraries/radspreadprocessing/features/number-formats)  
--- TelerikWpfApp1/MainWindow.xaml.cs
+++ TelerikWpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 using Telerik.Windows.Documents.Spreadsheet.FormatProviders;
 using Telerik.Windows.Documents.Spreadsheet.FormatProviders.OpenXml.Xlsx;
@@ -16,31 +17,44 @@ namespace TelerikWpfApp1
         public MainWindow()
         {
             InitializeComponent();
-
-            /* Step 1 - Create workbook */
-            Workbook workbook = new Workbook();
-            Worksheet worksheet = workbook.Worksheets.Add();
-
-            /* Step 2 - Set value of cell */
-            CellSelection selection = worksheet.Cells[1, 1]; //B2 cell 
-            selection.SetValue(07);
-
-            /* Step 3 - Export to xlsx */
-            string fileName = "SampleFile.xlsx";
-
-            IWorkbookFormatProvider formatProvider = new XlsxFormatProvider();
-
-            using (Stream output = new FileStream(fileName, FileMode.Create))
-            {
-                formatProvider.Export(workbook, output);
-            }
-
-            StatusLabel.Content = $"Excel document processing completed normally on {DateTime.Now}";
         }
 
         private void OpenDirectoryButton_Click(object sender, RoutedEventArgs e)
         {
             Process.Start(@"..\Debug");
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            OpenDirectoryButton.IsEnabled = false;
+            BusyIndicator.IsBusy = true;
+
+            await Task.Run(() =>
+            {
+                /* Step 1 - Create workbook */
+                Workbook workbook = new Workbook();
+                Worksheet worksheet = workbook.Worksheets.Add();
+
+                /* Step 2 - Set value of cell */
+                CellSelection selection = worksheet.Cells[1, 1]; //B2 cell 
+                selection.SetValue(0700);
+                selection.SetFormat(new CellValueFormat("\"0\"#"));
+
+                /* Step 3 - Export to xlsx */
+                string fileName = "SampleFile.xlsx";
+
+                IWorkbookFormatProvider formatProvider = new XlsxFormatProvider();
+
+                using (Stream output = new FileStream(fileName, FileMode.Create))
+                {
+                    formatProvider.Export(workbook, output);
+                }
+
+            });
+
+            StatusLabel.Content = $"Excel document processing completed normally on {DateTime.Now}";
+            BusyIndicator.IsBusy = false;
+            OpenDirectoryButton.IsEnabled = true;
         }
     }
 }
